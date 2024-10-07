8365f1bc26a1f1320f9902913d0316de2141d935
한승우 <tmddn_00@naver.com>
2024-10-07 22:41:38 +0900

    [Feat] 테스트 맵에 GameMode 적용

--- Source/VirtualIdol/Private/HSW/HSW_ThirdPersonCharacter.cpp
+++ Source/VirtualIdol/Private/HSW/HSW_ThirdPersonCharacter.cpp
@@ -6,6 +6,8 @@
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "../../../../Plugins/EnhancedInput/Source/EnhancedInput/Public/EnhancedInputSubsystems.h"
+#include "../../../../Plugins/EnhancedInput/Source/EnhancedInput/Public/EnhancedInputComponent.h"
 
 // Sets default values
 AHSW_ThirdPersonCharacter::AHSW_ThirdPersonCharacter()
@@ -68,17 +70,66 @@ void AHSW_ThirdPersonCharacter::Tick(float DeltaTime)
 // Called to bind functionality to input
 void AHSW_ThirdPersonCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	Super::SetupPlayerInputComponent ( PlayerInputComponent );
+
+	// Add Input Mapping Context
+	if (APlayerController* PlayerController = Cast<APlayerController> ( GetController ( ) ))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem> ( PlayerController->GetLocalPlayer ( ) ))
+		{
+			Subsystem->AddMappingContext ( DefaultMappingContext , 0 );
+		}
+	}
+
+	// Set up action bindings
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent> ( PlayerInputComponent )) {
+
+		// Jumping
+		EnhancedInputComponent->BindAction ( JumpAction , ETriggerEvent::Started , this , &ACharacter::Jump );
+		EnhancedInputComponent->BindAction ( JumpAction , ETriggerEvent::Completed , this , &ACharacter::StopJumping );
+
+		// Moving
+		EnhancedInputComponent->BindAction ( MoveAction , ETriggerEvent::Triggered , this , &AHSW_ThirdPersonCharacter::Move );
+
+		// Looking
+		EnhancedInputComponent->BindAction ( LookAction , ETriggerEvent::Triggered , this , &AHSW_ThirdPersonCharacter::Look );
+	}
 
 }
 
 void AHSW_ThirdPersonCharacter::Move ( const FInputActionValue& Value )
 {
+	// input is a Vector2D
+	FVector2D MovementVector = Value.Get<FVector2D> ( );
 
+	if (Controller != nullptr)
+	{
+		// find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation ( );
+		const FRotator YawRotation ( 0 , Rotation.Yaw , 0 );
+
+		// get forward vector
+		const FVector ForwardDirection = FRotationMatrix ( YawRotation ).GetUnitAxis ( EAxis::X );
+
+		// get right vector 
+		const FVector RightDirection = FRotationMatrix ( YawRotation ).GetUnitAxis ( EAxis::Y );
+
+		// add movement 
+		AddMovementInput ( ForwardDirection , MovementVector.Y );
+		AddMovementInput ( RightDirection , MovementVector.X );
+	}
 }
 
 void AHSW_ThirdPersonCharacter::Look ( const FInputActionValue& Value )
 {
+	// input is a Vector2D
+	FVector2D LookAxisVector = Value.Get<FVector2D> ( );
 
+	if (Controller != nullptr)
+	{
+		// add yaw and pitch input to controller
+		AddControllerYawInput ( LookAxisVector.X );
+		AddControllerPitchInput ( LookAxisVector.Y );
+	}
 }
 
