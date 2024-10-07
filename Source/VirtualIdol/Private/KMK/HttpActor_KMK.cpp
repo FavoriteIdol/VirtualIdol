c9cdb8c67050f662d371117ba6ce58d58cd3aa72
ryi01 <kkjmo3@naver.com>
2024-10-07 22:50:55 +0900

    [Feat] build파일 수정

--- Source/VirtualIdol/Private/KMK/HttpActor_KMK.cpp
+++ Source/VirtualIdol/Private/KMK/HttpActor_KMK.cpp
@@ -1,7 +1,6 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-
-#include "HttpActor_KMK.h"
+#include "KMK/HttpActor_KMK.h"
 #include "HttpModule.h"
 
 // Sets default values
