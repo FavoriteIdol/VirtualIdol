897bddd2f4c0093445b477113eb53a9e07807e19
한승우 <tmddn_00@naver.com>
2024-10-09 14:53:29 +0900

    [Add] HSW_GameMode

---
+++ Source/VirtualIdol/Private/HSW/HSW_AuditoriumGameMode.cpp
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HSW/HSW_AuditoriumGameMode.h"
+
+AHSW_AuditoriumGameMode::AHSW_AuditoriumGameMode ( )
+{
+	// 디폴트 폰 클래스 설정
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass ( TEXT ( "/Game/Project/Personal/HSW/Blueprints/HSW_BP_ThirdPerson" ) );
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
