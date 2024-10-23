c3d1a77668ef75469f4ebf8e6be8d6e855dee856
ryi01 <kkjmo3@naver.com>
2024-10-23 16:22:48 +0900

    [Edit] File

--- Source/VirtualIdol/Private/VirtualIdol.cpp
+++ Source/VirtualIdol/Private/VirtualIdol.cpp
@@ -1,6 +1,7 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
+﻿
 #include "VirtualIdol.h"
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VirtualIdol, "VirtualIdol" );
+
+DEFINE_LOG_CATEGORY(NetLog);
