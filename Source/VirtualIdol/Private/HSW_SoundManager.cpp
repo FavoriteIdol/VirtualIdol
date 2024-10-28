6ed15d23003f0cd0070d2831567ac8da541f2686
한승우 <tmddn_00@naver.com>
2024-10-28 19:52:02 +0900

    [Add] HSW_SoundManager

---
+++ Source/VirtualIdol/Private/HSW_SoundManager.cpp
@@ -0,0 +1,63 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HSW_SoundManager.h"
+#include "Net/UnrealNetwork.h"
+#include "Components/AudioComponent.h"
+#include "Sound/SoundBase.h"
+
+// Sets default values
+AHSW_SoundManager::AHSW_SoundManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AHSW_SoundManager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHSW_SoundManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AHSW_SoundManager::GetLifetimeReplicatedProps ( TArray<FLifetimeProperty>& OutLifetimeProps ) const
+{
+	Super::GetLifetimeReplicatedProps ( OutLifetimeProps );
+
+	DOREPLIFETIME ( AHSW_SoundManager , CurrentSongWav );
+	DOREPLIFETIME ( AHSW_SoundManager , CurrentSongComp );
+}
+
+void AHSW_SoundManager::ServerRPC_SetMusic_Implementation ( USoundBase* wavToSet )
+{
+	MulticastRPC_SetMusic(wavToSet );
+}
+
+void AHSW_SoundManager::MulticastRPC_SetMusic_Implementation ( USoundBase* wavToSet )
+{
+	CurrentSongComp->SetSound ( wavToSet );
+}
+
+void AHSW_SoundManager::ServerRPC_PlayMusic_Implementation ( UAudioComponent* MusicToPlay )
+{
+	MulticastRPC_PlayMusic( MusicToPlay );
+}
+
+void AHSW_SoundManager::MulticastRPC_PlayMusic_Implementation ( UAudioComponent* MusicToPlay )
+{
+	MusicToPlay->Play( );
+}
+
+void AHSW_SoundManager::PlaySound ( USoundBase* MusicToPlay )
+{
+
+}
+
---
+++ Source/VirtualIdol/Public/HSW_SoundManager.h
@@ -0,0 +1,51 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "HSW_SoundManager.generated.h"
+
+UCLASS()
+class VIRTUALIDOL_API AHSW_SoundManager : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AHSW_SoundManager();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void GetLifetimeReplicatedProps ( TArray<FLifetimeProperty>& OutLifetimeProps ) const override;
+
+	// 멤버 변수
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	TArray<class USoundBase* > WavArray;
+	UPROPERTY(Replicated )
+	class USoundBase* CurrentSongWav;
+	UPROPERTY(Replicated )
+	class UAudioComponent* CurrentSongComp;
+
+	// 기능
+	UFUNCTION(Server, Reliable )
+	void ServerRPC_SetMusic( USoundBase* wavToSet );
+
+	UFUNCTION(NetMulticast, Reliable )
+	void MulticastRPC_SetMusic( USoundBase* wavToSet );
+
+
+	UFUNCTION(Server, Reliable )
+	void ServerRPC_PlayMusic( UAudioComponent* MusicToPlay );
+
+	UFUNCTION(NetMulticast, Reliable )
+	void MulticastRPC_PlayMusic( UAudioComponent* MusicToPlay );
+
+	void PlaySound( USoundBase* MusicToPlay );
+};
